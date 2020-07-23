984a66d686d3df06832500622cceea7bf93a8400
Romain Braud <5973233-romainbraud@users.noreply.gitlab.com>
2020-07-23 11:33:15 +0200

    changes for Auth_correct

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import Amplify from 'aws-amplify';
-import Config from './aws-exports';
+import config from 'aws-exports';
 Amplify.configure(config);
 
 ReactDOM.render(
